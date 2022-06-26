b64394f83cefdddac472d27dc2e7df4b41152e5f
Fgsilva <fgsilva@ice.ufjf.br>
2022-06-25 21:58:37 -0300

    adiciona condições de vitória

--- app/src/main/java/com/example/pedrapapeltesouraspocklagarto/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeltesouraspocklagarto/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     public enum Jogada {
         PEDRA(0), PAPEL(1), TESOURA(2), SPOCK(3), LAGARTO(4);
         private final int valor;
+
         Jogada(int valor) {
             this.valor = valor;
         }
@@ -22,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
     public enum Resultado {
         DERROTA(-1), EMPATE(0), VITORIA(1);
         private final int valor;
-        Resultado(int valor){
+
+        Resultado(int valor) {
             this.valor = valor;
         }
     }
@@ -65,29 +67,29 @@ public class MainActivity extends AppCompatActivity {
         optionTextView = findViewById(R.id.optionTextView);
     }
 
-    public void buttonPedraClick(View view){
+    public void buttonPedraClick(View view) {
         rodada(Jogada.PEDRA);
     }
 
-    public void buttonPapelClick(View view){
+    public void buttonPapelClick(View view) {
         rodada(Jogada.PAPEL);
     }
 
-    public void buttonTesouraClick(View view){
+    public void buttonTesouraClick(View view) {
         rodada(Jogada.TESOURA);
     }
 
-    public void buttonSpockClick(View view){
+    public void buttonSpockClick(View view) {
         rodada(Jogada.SPOCK);
     }
 
-    public void buttonLagartoClick(View view){
+    public void buttonLagartoClick(View view) {
         rodada(Jogada.LAGARTO);
     }
 
-    public void rodada(Jogada jogada){
+    public void rodada(Jogada jogada) {
         Jogada jogadaComputador = Jogada.values()[dado.nextInt(4)];
-        switch (TABELA[jogada.valor][jogadaComputador.valor]){
+        switch (TABELA[jogada.valor][jogadaComputador.valor]) {
             case VITORIA:
                 pontosHumano += 3;
                 break;
@@ -95,15 +97,32 @@ public class MainActivity extends AppCompatActivity {
                 pontosComputador += 3;
                 break;
             case EMPATE:
-                pontosHumano ++;
-                pontosComputador ++;
+                pontosHumano++;
+                pontosComputador++;
                 break;
         }
         atualizaStatus();
     }
 
-    private void atualizaStatus(){
+    private void iniciaTorneio(){
+        pontosComputador = 0;
+        pontosHumano = 0;
+    }
+
+    private void atualizaStatus() {
         progressBarComputador.setProgress(pontosComputador);
         progressBarHumano.setProgress(pontosHumano);
+        if (pontosHumano < 15 && pontosComputador < 15) {
+            optionTextView.setText("Escolha uma opção.");
+        } else if (pontosHumano >= 15 && pontosComputador < 15) {
+            optionTextView.setText("O humano venceu o torneio.");
+            iniciaTorneio();
+        } else if (pontosHumano < 15 && pontosComputador >= 15) {
+            optionTextView.setText("O computador venceu o torneio.");
+            iniciaTorneio();
+        } else {
+            optionTextView.setText("O torneio terminou empatado.");
+            iniciaTorneio();
+        }
     }
 }
